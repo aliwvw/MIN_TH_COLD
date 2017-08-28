315a22196a490f6bd2f440e8a366e3ae5ce6930c
aliwvw <31378469+aliwvw@users.noreply.github.com>
2017-08-28 13:35:03 +0300

    Update ar-info.lua

--- plugins/ar-info.lua
+++ plugins/ar-info.lua
@@ -47,7 +47,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'❣ عدد الرسائل المرسله : '..user_info_msgs..'\n\n'
-  text = text..'❣ #المطور : @fuck_8_you\n❣ #قناة_البوت : @help_telp'
+  text = text..'❣ #المطور : @ali_moom\n❣ #قناة_البوت : @MIN_TH_COLD'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, ' Username not found.', ok_cb, false)
@@ -88,7 +88,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'❣ عدد الرسائل المرسله : '..user_info_msgs..'\n\n'
-  text = text..'❣ #المطور : @fuck_8_you\n❣ #قناة_البوت : @help_telp'
+  text = text..'❣ #المطور : @ali_moom\n❣ #قناة_البوت : @MIN_TH_COLD'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
@@ -127,7 +127,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'❣ عدد الرسائل المرسله : '..user_info_msgs..'\n\n'
-  text = text..'❣ #Dev : @jozef3 \n❣ #قناة_البوت : @fr300t'
+  text = text..'❣ #Dev : @ali_moom \n❣ #قناة_البوت : @MIN_TH_COLD'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -202,7 +202,7 @@ local function run(msg, matches)
 	 text = text..'❣ اسم المجموعه : '..msg.to.title..'\n'
      text = text..'❣ ايدي المجموعه : '..msg.to.id
     end
-	text = text..'\n\n❣ #المطور : @jozef3❣ #قناة_البوت : @fr300t'
+	text = text..'\n\n❣ #المطور : @ali_moom ❣ #قناة_البوت : @MIN_TH_COLD'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
